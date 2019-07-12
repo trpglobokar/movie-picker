5bf555502f6efc77cc76d99cb24bb0445bafa482
Rocky PG <trp.globokar@gmail.com>
2019-07-11 22:09:33 -0700

    Updates

---
+++ precache-manifest.59f446ab1993b9603f2a83a3b2bce3a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7465d731e074fedd2371996f45736cd1",
+    "url": "/movie-picker/index.html"
+  },
+  {
+    "revision": "bd15d3e64204dc5dca94",
+    "url": "/movie-picker/static/css/main.2fbb6750.chunk.css"
+  },
+  {
+    "revision": "2609460557e4487af1a2",
+    "url": "/movie-picker/static/js/2.d6822570.chunk.js"
+  },
+  {
+    "revision": "bd15d3e64204dc5dca94",
+    "url": "/movie-picker/static/js/main.fce6b879.chunk.js"
+  },
+  {
+    "revision": "51eea1178bf52f21dd3b",
+    "url": "/movie-picker/static/js/runtime~main.fb397a83.js"
+  }
+]);
