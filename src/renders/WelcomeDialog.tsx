ebb07fc6b89c5b8ddea057095ee47f30625e2a6b
Rocky Pristou-Globokar <trp.globokar@gmail.com>
2020-02-09 23:35:55 -0800

    feat(WelcomeDialog): added 'Get Started' button

--- src/renders/WelcomeDialog.tsx
+++ src/renders/WelcomeDialog.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import {
+  Button,
   Dialog,
   DialogTitle,
 } from "@material-ui/core"
@@ -24,7 +25,7 @@ class WelcomeDialog extends Component<WelcomeProps, WelcomeState> {
     super(props)
 
     this.state = {
-      modalOpen: false, //modalOpen: true,
+      modalOpen: true,
     }
   }
 
@@ -45,11 +46,15 @@ class WelcomeDialog extends Component<WelcomeProps, WelcomeState> {
           Welcome to the Movie Picker
         </DialogTitle>
         <DialogContent>
-          <p>Ever sat down with a giant collection of movies you know you want to watch but no idea how to pick just one?</p>
-          <p>Are you and a friend constantly saying things like "what do you mean you haven't watched the Princess Bride?!", but when you're both on the couch you can barely remember one title, let alone the dozen others you've been dying to share?</p>
+          <p>Ever sat down with a giant collection of movies you've been wanting to watch, but you have no idea how to pick just one?</p>
           <p>If so, this tool is for you.</p>
           <p>Load up a list of pre-selected titles and start filtering by whatever genre you might be in the mood for. If you're still stuck with indecision, use the "Choose For Me" button to randomly select a film from the final results.</p>
-          <SmallP>(NOTE: ability to add custom user lists coming soon)</SmallP>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.handleClose}>
+              Get Started
+          </Button>
         </DialogContent>
       </Dialog>
     )
